8dbe36058f60eda45e2d3a9db3e006cc030bea4e
Adam Gilmour <AdamGilmour3@users.noreply.github.com>
2024-08-05 16:11:44 +0100

    Reformatting and vive update
    Code reformatted for improved readability, updated to read filtered vive topics

--- src/scripts/AtoB.py
+++ src/scripts/AtoB.py
@@ -7,13 +7,16 @@ from navic_messages.msg import ScanlinkControl
 from geometry_msgs.msg import PoseWithCovarianceStamped
 #from strath_meca_control.msg import MecaVelControl
 from geometry_msgs.msg import Twist, TwistStamped
+# import tf functionality
+import tf2_ros
+import geometry_msgs.msg
 
 import time
 
 class RobotController:
     def __init__(self):
-        self.vive_pose_crawler = rospy.Subscriber("/vive/LHR_EA821C01_pose", PoseWithCovarianceStamped , self.AtoB_navic_callback, queue_size=1)
-        # self.vive_pose_plate = rospy.Subscriber("/vive/LHR_6727695C_pose", PoseWithCovarianceStamped , self.vive_plate_callback, queue_size=1)        
+        self.vive_pose_crawler = rospy.Subscriber("/vive/LHR_EA821C01_pose_filt", PoseWithCovarianceStamped , self.AtoB_navic_callback, queue_size=1)
+        self.vive_pose_plate = rospy.Subscriber("/vive/LHR_6727695C_pose_filt", PoseWithCovarianceStamped , self.vive_plate_callback, queue_size=1)        
         self.navic_publisher = rospy.Publisher("/scanlink_control", ScanlinkControl, queue_size=1)
         self.timer_crawler_update = rospy.Timer(rospy.Duration(1.0/25.0), self.update_speed_callback)
         self.speed = 0
@@ -22,38 +25,49 @@ class RobotController:
         self.steer_ratio = 100
         self.speed_ratio = 1000    
 
-        self.start_pose = 0
+        self.start_pose = []
         self.forward = True
 
         self.plate_pose = PoseWithCovarianceStamped()
         self.navic_pose = PoseWithCovarianceStamped()
+
+        self.trans = PoseWithCovarianceStamped()
+        self.tfBuffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tfBuffer)
+        self.listener3 = tf2_ros.TransformListener(self.tfBuffer)
+        self.navic_rel_plate = rospy.Publisher("navic_rel_plate", PoseWithCovarianceStamped, queue_size=1)
     
     def vive_plate_callback(self, input):
         self.plate_pose = input
 
 
     def AtoB_navic_callback(self, input):
-        navic_x = input.pose.pose.position.x
-        navic_y = input.pose.pose.position.y
-        navic_z = input.pose.pose.position.z
-        if self.start_pose == 0:
+
+        self.trans = self.tfBuffer.lookup_transform("LHR_6727695C_pose_filt", "LHR_EA821C01_pose_filt", rospy.Time())
+        # print(self.trans)
+
+        navic_x = self.trans.transform.translation.x
+        navic_y = self.trans.transform.translation.y
+        navic_z = self.trans.transform.translation.z
+        
+        if self.start_pose == []:
             self.start_pose = [navic_x, navic_y, navic_z]
             print("Initial Pose: " + str(self.start_pose))  
 
         if self.forward:
-            self.speed = 1
+            self.speed = 0.5
             self.steer = 0
             print("Forward Motion: " + str(["%.4f" % navic_x, "%.4f" % navic_y, "%.4f" % navic_z]))
-            if navic_x >= self.start_pose[0]+0.05:
+            if navic_x <= self.start_pose[0]-0.1:
                 self.forward = False
                 self.speed = 0
                 print("Hold Position...")
                 time.sleep(1)
         else:
-            self.speed = -1
+            self.speed = -0.5
             self.steer = 0
             print("Reverse Motion: " + str(["%.4f" % navic_x, "%.4f" % navic_y, "%.4f" % navic_z]))
-            if navic_x <= self.start_pose[0]:
+            if navic_x >= self.start_pose[0]:
                 self.forward = True
                 self.speed = 0
                 print("Hold Position...")
