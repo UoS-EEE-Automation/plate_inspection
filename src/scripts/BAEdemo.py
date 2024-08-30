bff44cfc99600c03e5b78081a7895122a20de2fe
Adam Gilmour <AdamGilmour3@users.noreply.github.com>
2024-08-30 13:05:17 +0100

    Code Restructre
    Added definitions to compartmentalise code better

--- src/scripts/BAEdemo.py
+++ src/scripts/BAEdemo.py
@@ -102,7 +102,7 @@ class RobotController:
             self.probe_target_transformStamped.transform.rotation.y = probe_wp_q[2]
             self.probe_target_transformStamped.transform.rotation.z = probe_wp_q[3]
 
-            # Publish the current navic target frame
+            ################################################################## Publish the current navic target frame
             self.navic_target_transformStamped.header.stamp = rospy.Time.now()
             self.navic_target_transformStamped.header.frame_id = "LHR_6727695C_pose_filt"
             self.navic_target_transformStamped.child_frame_id = "navic_target"
@@ -123,6 +123,11 @@ class RobotController:
             self.navic_target_transformStamped.transform.rotation.z = navic_wp_q[3]
 
     def waypoint_navic_callback(self, input):
+        self.read_navic_position()
+
+        self.debug_msg_count += 1    
+
+    def read_navic_position(self):
         if self.count >= 10:
             self.trans = self.tfBuffer.lookup_transform("LHR_6727695C_pose_filt", "navic_control_link", rospy.Time())
         else:
@@ -130,118 +135,127 @@ class RobotController:
             return
 
         if self.waypoints_generated == 1:
+            self.navic_x = self.trans.transform.translation.x*1000
+            self.navic_y = self.trans.transform.translation.y*1000
+            self.navic_z = self.trans.transform.translation.z*1000
+            self.navic_qx = self.trans.transform.rotation.x
+            self.navic_qy = self.trans.transform.rotation.y
+            self.navic_qz = self.trans.transform.rotation.z
+            self.navic_qw = self.trans.transform.rotation.w
 
-            navic_x = self.trans.transform.translation.x*1000
-            navic_y = self.trans.transform.translation.y*1000
-            navic_z = self.trans.transform.translation.z*1000
-            navic_qx = self.trans.transform.rotation.x
-            navic_qy = self.trans.transform.rotation.y
-            navic_qz = self.trans.transform.rotation.z
-            navic_qw = self.trans.transform.rotation.w
+            self.calculate_wheel_speeds()
 
-            if self.insp_stage in range(1,4):
+    def calculate_wheel_speeds(self):
+        if self.insp_stage in range(1,4):
 
-                # Calculate Steer
-                navic_skew = euler_from_quaternion([navic_qx, navic_qy, navic_qz, navic_qw])
-                navic_yaw = (navic_skew[2] * 180/math.pi) % 360 - 180
-                # Calculate target skew based upon fixed waypoint:
-                # target_skew = math.atan2(self.navic_wp[-self.insp_stage][1] - navic_y, self.navic_wp[-self.insp_stage][0] - navic_x) * 180/math.pi  % 360 - 180
-                # Calculate skew based upon point 200mm in front of crawler:
-                target_skew = math.atan2(self.navic_wp[-self.insp_stage][1] - navic_y, - 200) * 180/math.pi  % 360 - 180
-                theta_err = (target_skew - navic_yaw) 
+            # Calculate Steer
+            navic_skew = euler_from_quaternion([self.navic_qx, self.navic_qy, self.navic_qz, self.navic_qw])
+            navic_yaw = (navic_skew[2] * 180/math.pi) % 360 - 180
+            # Calculate target skew based upon fixed waypoint:
+            # target_skew = math.atan2(self.navic_wp[-self.insp_stage][1] - navic_y, self.navic_wp[-self.insp_stage][0] - navic_x) * 180/math.pi  % 360 - 180
+            # Calculate skew based upon point 200mm in front of crawler:
+            target_skew = math.atan2(self.navic_wp[-self.insp_stage][1] - self.navic_y, - 200) * 180/math.pi  % 360 - 180
+            theta_err = (target_skew - navic_yaw) 
 
-                # print("Navic Skew: " + str(navic_yaw))
-                # print("Target Skew: " + str(target_skew))
-                # print("Skew Error: " + str(theta_err))
-                # print("Navic X: " + str(navic_x))
-                # print("Navic Y: " + str(navic_y))
-                # print("Waypoint X: " + str(self.navic_wp[-1][0]))
-                # print("Waypoint Y: " + str(self.navic_wp[-1][1]))
-                # print("")
+            # print("Navic Skew: " + str(navic_yaw))
+            # print("Target Skew: " + str(target_skew))
+            # print("Skew Error: " + str(theta_err))
+            # print("Navic X: " + str(navic_x))
+            # print("Navic Y: " + str(navic_y))
+            # print("Waypoint X: " + str(self.navic_wp[-1][0]))
+            # print("Waypoint Y: " + str(self.navic_wp[-1][1]))
+            # print("")
 
-                if navic_x > self.navic_wp[-self.insp_stage][0] and self.stop == 0:
-                    self.speed = 0.5
-                    # if abs(navic_x - self.navic_wp[-self.insp_stage][0]) < 100:
-                    #     self.speed = 0.2
-                    if abs(navic_x - self.navic_wp[-self.insp_stage][0]) <= 100 and abs(navic_x - self.navic_wp[-self.insp_stage][0]) > 10:
-                        self.speed = abs(navic_x - self.navic_wp[-self.insp_stage][0])/100 * self.speed
-                    elif abs(navic_x - self.navic_wp[-self.insp_stage][0]) < 10:
-                        self.speed = 0.05
+            if self.navic_x > self.navic_wp[-self.insp_stage][0] and self.stop == 0:
+                self.speed = 0.5
+                # if abs(navic_x - self.navic_wp[-self.insp_stage][0]) < 100:
+                #     self.speed = 0.2
+                if abs(self.navic_x - self.navic_wp[-self.insp_stage][0]) <= 100 and abs(self.navic_x - self.navic_wp[-self.insp_stage][0]) > 10:
+                    self.speed = abs(self.navic_x - self.navic_wp[-self.insp_stage][0])/100 * self.speed
+                elif abs(self.navic_x - self.navic_wp[-self.insp_stage][0]) < 10:
+                    self.speed = 0.05
 
-                    self.steer = theta_err/-15
-                    if abs(self.steer) > 0.8:
-                        if self.steer < 0:
-                            self.steer = -0.8
-                        elif self.steer > 0:
-                            self.steer = 0.8
-                    # print("Moving forward...   Steer: " + str(self.steer*self.steer_ratio))
-                    rospy.logdebug("Moving forward...   Speed: " + str(self.speed*self.speed_ratio) + "   Steer: " + str(self.steer*self.steer_ratio))
+                self.steer = theta_err/-15
+                if abs(self.steer) > 0.8:
+                    if self.steer < 0:
+                        self.steer = -0.8
+                    elif self.steer > 0:
+                        self.steer = 0.8
+                # print("Moving forward...   Steer: " + str(self.steer*self.steer_ratio))
+                if self.debug_msg_count%50 == 0:
+                    rospy.logdebug("Moving forward...   Speed: " + str(int(self.speed*self.speed_ratio)) + "   Steer: " + str(int(self.steer*self.steer_ratio)))
 
-                elif navic_x <= self.navic_wp[-self.insp_stage][0] and self.stop == 0:
-                    self.speed = 0
-                    self.steer = 0
-                    self.stop = 1
-                    # print("Stopping...")
-                    rospy.logdebug("Stopping...")
-                else:
-                    # print("Waypoint " + str(self.insp_stage) + " reached... Pausing for 3s.")
-                    rospy.logdebug("Waypoint " + str(self.insp_stage) + " reached... Approaching sample....")
+            elif self.navic_x <= self.navic_wp[-self.insp_stage][0] and self.stop == 0:
+                self.speed = 0
+                self.steer = 0
+                self.stop = 1
+                # print("Stopping...")
+                rospy.logdebug("Stopping...")
+            else:
+                # print("Waypoint " + str(self.insp_stage) + " reached... Pausing for 3s.")
+                rospy.logdebug("Waypoint " + str(self.insp_stage) + " reached... Approaching sample....")
 
-                    # Calculate transform between Meca base and probe start waypoint, this is fed through to the robot controller                    
-                    self.trans_probe = self.tfBuffer.lookup_transform("probe_target", "meca_base_link", rospy.Time()) # Provides correct angle
-                    self.trans_probe_2 = self.tfBuffer.lookup_transform("meca_base_link", "probe_target", rospy.Time()) # Provides correct translation
+                # Calculate transform between Meca base and probe start waypoint, this is fed through to the robot controller                    
+                self.trans_probe = self.tfBuffer.lookup_transform("probe_target", "meca_base_link", rospy.Time()) # Provides correct angle
+                self.trans_probe_2 = self.tfBuffer.lookup_transform("meca_base_link", "probe_target", rospy.Time()) # Provides correct translation
 
-                    target_eul = transforms3d.euler.quat2euler(
-                        [
-                        self.trans_probe.transform.rotation.w,
-                        self.trans_probe.transform.rotation.x,
-                        self.trans_probe.transform.rotation.y,
-                        self.trans_probe.transform.rotation.z
-                        ]
-                    )
-                    target_eul = numpy.array(target_eul)*-180/math.pi
+                target_eul = transforms3d.euler.quat2euler(
+                    [
+                    self.trans_probe.transform.rotation.w,
+                    self.trans_probe.transform.rotation.x,
+                    self.trans_probe.transform.rotation.y,
+                    self.trans_probe.transform.rotation.z
+                    ]
+                )
+                target_eul = numpy.array(target_eul)*-180/math.pi
 
-                    # Send Action to conduct scan
-                    goal = scanGoal()
+                # Send Action to conduct scan
+                goal = scanGoal()
 
-                    probe_target_pose = [
-                        self.trans_probe_2.transform.translation.x*1000-20, # Convert from m to mm
-                        self.trans_probe_2.transform.translation.y*1000,
-                        self.trans_probe_2.transform.translation.z*1000,
-                        target_eul[0], # Convert from rad to deg
-                        0, #target_eul[1] We want to set this value ourself
-                        target_eul[2]
-                        ]
-                    
-                    goal.scan_start = probe_target_pose
-                    self.act_client.send_goal(goal)
-                    self.act_client.wait_for_result()
-                    rospy.logdebug("Scan " + str(self.insp_stage) + " complete.")
-                    # goal.scan_start = 0
-
-                    # Scan Complete
-
-                    rospy.sleep(3)
-                    self.insp_stage += 1
-                    self.stop = 0
-            elif self.insp_stage == 4:
-                if navic_x < 1100:
-                    # print("Returning to home...")
-                    rospy.logdebug("Returning to home...")
-                    self.speed = -0.8
-                    self.steer = 0
-                elif navic_x >= 1100:
-                    # print("Inspection Complete...")
-                    self.speed = 0
-                    self.steer = 0
-                    self.insp_stage += 1
-            elif self.insp_stage == 5:
-                    rospy.logdebug("Inspection Complete...")
-                    self.speed = 0
-                    self.steer = 0
-                    
+                probe_target_pose = [
+                    self.trans_probe_2.transform.translation.x*1000-20, # Convert from m to mm
+                    self.trans_probe_2.transform.translation.y*1000,
+                    self.trans_probe_2.transform.translation.z*1000,
+                    target_eul[0], # Convert from rad to deg
+                    0, #target_eul[1] We want to set this value ourself
+                    target_eul[2]
+                    ]
                 
-    def generate_waypoints(self, *input):
+                goal.scan_start_pose = probe_target_pose
+                goal.scan_length = 240
+                self.act_client.send_goal(goal)
+                self.act_client.wait_for_result()
+                rospy.logdebug("Scan " + str(self.insp_stage) + " complete.")
+                # goal.scan_start = 0
+
+                # Scan Complete
+
+                rospy.sleep(3)
+                self.insp_stage += 1
+                self.stop = 0
+        elif self.insp_stage == 4:
+            self.go_home()
+
+    def go_home(self):    
+        if self.navic_x < 1100:
+            # print("Returning to home...")
+            if self.debug_msg_count%50 == 0:
+                rospy.logdebug("Returning to home...")
+            self.speed = -0.8
+            self.steer = 0
+        elif self.navic_x >= 1100:
+            # print("Inspection Complete...")
+            self.speed = 0
+            self.steer = 0
+            self.insp_stage += 1
+        elif self.insp_stage == 5:
+            if self.debug_msg_count%50 == 0:
+                rospy.logdebug("Inspection Complete...")
+            self.speed = 0
+            self.steer = 0
+                        
+                
+    def generate_waypoints(self):
         
         for i in range(1,4,1):
             if i == 1:
@@ -275,7 +289,6 @@ class RobotController:
                 ]
                 )
         self.waypoints_generated = 1
-        #while not rospy.is_shutdown():
         
     def update_speed_callback(self, timer_crawler):
         if self.waypoints_generated == 1:
